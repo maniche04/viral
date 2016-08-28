3e6bf2dc84b7ba87742c432134f1cd6609e2eedc
Manish Bhattarai <manish.ilam@gmail.com>
2016-08-28 19:28:48 +0400

    Article

--- resources/views/c1.php
+++ resources/views/c1.php
@@ -3,31 +3,7 @@
 	echo $test;
 
 	/*
-		C1
-		----
-		Ethics
-		Lets take the case of Tax Avoidance into perspective. Statistics show that companies that do not engage in tax avoidance are effectively pegging themselves back. Despite the widely known cases of GE, Google and other big companies, there has been very less that the state authorities could do. Afterall, Tax Avoidance is something legal. Within the debate of whether tax avoidance falls under negative CSR, it can be argued that the only thing that can bind the companeis to pay tax is their ethics, as reflected by company. Again, the argument that companies cannot make ethical decisions is rebutted by these examples. Its rather a choice of the companies rather than the inherent factor.
-
-		Economic
-		Comapanies that have positive reputation in terms of CRS were considered 'secure investment' by the investors. This would mean that the social acceptance of company would lead to easy reach to capital injections.
-
-		It could be argued keeping in view the case of Nike and Rebook that a company that takes the chances and acts as a leader in new CSR initiatives would stay ahead of the rest of the competition, while companies that might not intend to initiate the initiatives ultimately would have to follow it. Thus, it could be argued that CSR might not be entirely voluntary afterall, social expectations can force a company to align itself with CSR. If that's the case, why not be a leader rather than a follower?
-
-		It is often said that CSR brings in additional cost to the business. Consider the case of company this, where this happened and this was made. Its clear that companies that engage in environment friendly innovation not just gain themselves a 'CSR friendly' tag, but also earn efficiency in terms of costs.
-
-		Compliance / Risk Management
-		It is widely accepted that companies are accomodating CSR as a mechanism to build upon the 'intangibles', reputation, brand value and goodwill. 
-
-		As seen from examples, companies need not just maintain the license to operate by complying with regulations, but they need to comply with the expectations of the society and public at large. It is vital for a company to establish among the society that the company. However, there's a concern in if companies are using CSR to build a positive reputation or wipe the negative reputation that they have earned through other negative actions. Cases of GE who initiates CSR activities can certainly be considered something similar.
-	
-		The recent study suggets ever growing emphasis of investors and finance community on the use of accountability and sustainability reports for companies. Sustainability standards are expected to be madatory throughout the globe very soon. Singapore, for example has already set the target of 2020 for the mandatory implementation of sustainability reporting. (ACCA Accounting & Business.) Considering these incremental shift from voluntary CSR to mandatory CSR approaches and reporting practices, it would be appropriate to argue that businesses from a compliance view need to make themselfs familiar with CSR and its reporting mechanisms.
-
-
-		RISK AND COMPLIANCE
-	
-		http://www.carrotsandsticks.net/about-carrots-and-sticks/
-		Over the last three years, the total number of sustainability reporting instruments has grown rapidly and significantly worldwide. (Carrots & Sticks published by KPMG International, GRI, United Nations Environment Programme (UNEP), 2016). The report identified 383 reporting initiatives across 64 countries in 2016 compared to 180 instruments in 44 countries in 2013, with the primary reason cited as increasing expectations from businesses to report information not only on their financial performance but also on material aspects of their non-financial performance.
-
+		Doubts about the reliability of foreign financial statements is considered as one of the major detriment to foreign investment.
 		
 
 
